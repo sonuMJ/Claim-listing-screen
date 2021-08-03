52272916cda81c1bf1d0d68a4fd89ed72645b0d0
Sonu Jose <sonu@gmail.com>
2021-08-03 14:42:43 +0530

    claims routes update

--- routes/claimRoutes.js
+++ routes/claimRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const claimController = require('../controller/claimController');
+const jwtToken = require('../security/Jwttoken')
 
 /**
  * @method GET
  * @param params page no, limit(no of claims per page)
  */
-router.get("/listclaims/:page/:limit",claimController.getClaims)
+router.get("/listclaims/:page/:limit",jwtToken.AuthenticateUser,claimController.getClaims)
 
 module.exports = router;
